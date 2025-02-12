310f7686eb0f79ac08ec8aa3bf36464848108e3c
asmitmans <a.smitmans@gmail.com>
2025-02-12 18:39:58 -0300

    SQL OperationalError

--- app/repositories/sensor_repository.py
+++ app/repositories/sensor_repository.py
@@ -1,3 +1,4 @@
+from sqlalchemy.exc import OperationalError
 from app import db
 from app.models.sensor_data import SensorData
 import logging
@@ -8,10 +9,12 @@ def save_sensor_data(sensor_data):
     try:
         db.session.add(sensor_data)
         db.session.commit()
+    except OperationalError:
+        logger.error("Error de conexión con la base de datos. Verifica si PostgreSQL está en ejecución.")
+        raise Exception("No se pudo conectar a la base de datos.")  # Lanzar excepción controlada
     except Exception as e:
-        db.session.rollback()
-        logger.error(f"Error al guardar en BD: {e}")
-        raise Exception("Error interno al guardar en la base de datos.")
+        logger.error(f"Error interno al guardar en la base de datos: {str(e)}")
+        raise Exception("Error interno al guardar en la base de datos.")  # Lanzar excepción controlada
 
 def get_all_sensor_data():
     try:
