dde65f1f904c13c695aec47048379c2e5188f697
asmitmans <a.smitmans@gmail.com>
2025-02-13 22:58:44 -0300

    Refactor: parametriza host y puerto en .env, añade prefijo /api

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
+from app.config import Config
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=Config.FLASK_RUN_HOST, port=Config.FLASK_RUN_PORT)
